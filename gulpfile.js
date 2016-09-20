65c0c35dde3da346215f45e51354ead1f32e915e
Andrew Dibb <andrewdibb@gmail.com>
2016-09-20 17:45:47 -0500

    get pug up and running

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var pug = require('gulp-pug');
 
 gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
@@ -7,7 +8,15 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('./css/'))
 });
 
+gulp.task('pug', function(done) {  
+  gulp.src('./pug/**/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('./'))
+    .on('end', done);
+});
+
 //Watch task
 gulp.task('default',function() {
     gulp.watch('sass/**/*.scss',['styles']);
+    gulp.watch('pug/**/*.pug',['pug']);
 });
